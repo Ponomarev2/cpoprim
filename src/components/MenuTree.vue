962bd11115a323013ec1afc411d975d5ec409a16
Vasily Ponomarev <vasilyponomarev233@gmail.com>
2023-05-09 21:20:56 +0300

    add blind mode

--- src/components/MenuTree.vue
+++ src/components/MenuTree.vue
@@ -1,73 +1,133 @@
 <script setup>
-import MenuElem from './MenuElem.vue'
-import Menu from './Menu.vue'
-
 import { ref, reactive, onMounted } from 'vue'
 
+const menuTree = [
+  {name: 'Новости', subMenu: []},
+  {name: 'Сведения об организации', subMenu: [
+    {name: 'Охрана труда', subMenu: []},
+    {name: 'Основные сведения', subMenu: []},
+    {name: 'Противодействие коррупции', subMenu: []},
+    {name: 'Структура', subMenu: []},
+    {name: 'Профсоюз', subMenu: []},
+    {name: 'Состав сотрудников и контактные данные', subMenu: []},
+  ]},
+  {name: 'Направления работы', subMenu: [
+    {name: 'Мероприятия', subMenu: []},
+    {name: 'Профориентация', subMenu: []},
+    {name: 'Олимпиада учителей', subMenu: []},
+    {name: 'Казачье образование', subMenu: []},
+    {name: 'Организация перевозок', subMenu: []},
+  ]},
+  {name: 'Деятельность', subMenu: []},
+  {name: 'Планы', subMenu: []},
+  {name: 'Горячиe линии', subMenu: []},
+  {name: 'Контакты', subMenu: []},
+];
 
-function koo(){
-  console.log(document.getElementById('menu'));
-  console.log(document.getElementById('menu').offsetHeight);
+
+function onMouseOver({target}){
+  // console.log('over', target);
+  if(target.parentNode.childNodes.length > 1 && target.parentNode.childNodes[1].className === 'sub-menu'){
+    target.parentNode.childNodes[1].style.visibility = 'visible';
+  }
 }
 
-
-function DFS(node, acc){
-  const tree = node.childNodes;
-  if(node.class === "menu" || node.class === "menu-tab"){
-    node.style.top = -acc + 'px';
+function onMouseLeave({target}){
+  // console.log('leave', target);
+  if(target.childNodes.length > 1 && target.className === 'tab'){
+    target.childNodes[1].style.visibility = 'hidden';
   }
+}
 
+function DFS(node){
+  const tree = node.childNodes;
+  if(node.className === "sub-menu"){
+    node.style.top = -node.parentNode.childNodes[0].offsetHeight + 'px';
+  }
   if(tree.length === 0 && node.data !== ""){
-    acc += node.parentNode.offsetHeight;
-    return acc;
+    return;
   }else{
     for (let index = 0; index < tree.length; index++) {
       const elem = tree[index];
-      acc = DFS(elem, acc);
+      DFS(elem);
     }
   }
-  return acc;
 }
 
 
 onMounted(() => {
-  const tree = document.getElementById('menu').childNodes;
-  DFS(document.getElementById('menu'), 0);
-  // let accHeight = 0;
-  // for (let index = 0; index < tree.length; index++) {
-  //   const elem = tree[index];
-  //   elem.style.top = -accHeight + 'px';
-  //   console.log(elem);
-  //   accHeight = DFS(elem, accHeight);
-  //   console.log(accHeight);
-  //   break;
-  // }
+  const top = document.getElementById('menu');
+  DFS(top);
+  const tree = top.childNodes;
+  let accHeight = 0;
+  console.log(tree);
+  for (let index = 0; index < tree.length; index++) {
+    const element = tree[index];
+    console.log(element);
+    console.log(accHeight);
+    if(element.className === 'tab'){
+      element.style.top = -accHeight +'px';
+    }
+    if(element.childNodes.length > 1){
+      if(element.childNodes[1].className === 'sub-menu'){
+        accHeight += element.childNodes[1].offsetHeight;
+      }
+    }
+  }
 })
 
 </script>
 
 
 <template>
-  <div>
-    <button @click="koo">hjk</button>
-    <div class="menu" id="menu">
-      <div class="menu-tab">
-        <MenuElem :label="'tab 1 level 1'"/>
-        <div class="sub-menu">
-          <Menu/>
+  <div class="menu" id="menu">
+    <div v-for="(item, i) in menuTree" class="tab" @mouseover="onMouseOver" @mouseleave="onMouseLeave">
+      <div class="menu-elem">
+        {{ item.name }}<div v-if="item.subMenu.length > 0" class="sub-menu-icon">▶️</div>
+      </div>
+      <div v-if="item.subMenu.length > 0" class="sub-menu">
+        <div v-for="(item2, j) in item.subMenu" class="tab" @mouseleave="onMouseLeave">
+          <div class="menu-elem">
+            {{ item2.name }}<div v-if="item2.subMenu.length > 0" class="sub-menu-icon">▶️</div>
+          </div>
+          <div v-if="item2.subMenu.length > 0" class="sub-menu">
+            <div v-for="(item3, k) in item2.subMenu" class="tab">
+              <div class="menu-elem">{{ item3.name }}</div>
+            </div>
+          </div>
         </div>
       </div>
+    </div>
 
-      <div class="menu-tab">
-        <MenuElem :label="'tab 2 level 1'"/>
-        <!-- <div class="sub-menu">
-          <Menu/>
-        </div> -->
+
+    <div class="tab" @mouseover="onMouseOver" @mouseleave="onMouseLeave">
+      <div class="menu-elem">'tab 1 level 1'</div>
+      <div class="sub-menu" >
+        <div class="tab">
+          <div class="menu-elem">tab 1 level 2</div>
+        </div>
+        <div class="tab" @mouseleave="onMouseLeave">
+          <div class="menu-elem">tab 2 level 2</div>
+          <div class="sub-menu">
+            <div class="tab">
+              <div class="menu-elem">tab 3 level 2</div>
+            </div>
+          </div>
+        </div>
       </div>
-      
+    </div>
+    <div class="tab" @mouseover="onMouseOver" @mouseleave="onMouseLeave">
+      <div class="menu-elem">tab 2 level 1</div>
+      <div class="sub-menu" >
+        <div class="tab">
+          <div v-for="(item, i) in [1,2,3,4]"
+            class="menu-elem"
+            :key="item"
+          >{{ 'elem' + item }}</div>
+        </div>
+      </div>
     </div>
   </div>
-  
 </template>
 
 
@@ -79,15 +139,48 @@ onMounted(() => {
   flex-direction: column;
   z-index: 3;
   width: 100%;
-  height: 1000px;
+  height: max-content;
+  background-color: white;
+  font-family: 'PT Sans', sans-serif;
+  font-weight: normal;
 }
 
 .sub-menu{
-  left: 230px;
+  left: calc(100%);
+  visibility: hidden;
 }
 
-.menu-tab{
+.tab{
   position: relative;
 }
+
+.menu-elem{
+  display: flex;
+  width: 100%;
+  height: min-content;
+  color: rgb(9, 2, 40);
+  font-size:larger;
+  font-weight: normal;
+  line-height: 1.3em;
+  border: 2px solid gray;
+  cursor: pointer;
+  padding: 5px;
+  padding-left: 7px;
+  background-color: white;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.menu-elem:hover{
+  background-color: lightgrey;
+  transition: background-color 200ms;
+}
+
+.sub-menu-icon{
+  border: none;
+  outline: none;
+  
+}
+
 
 </style>
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,21 +1,27 @@
 <script setup>
 import MenuTree from './MenuTree.vue'
-import Menu from './Menu.vue'
+import Post from './Post.vue'
+import PageTitle from './PageTitle.vue'
 
 </script>
 
 
 <template>
   <div class="page">
-    <div class="head">
-      <div class="logo">МКУ "Центр поддержки образования" МО Приморско-Ахтарский район</div>
+    <div class="top">
+      <div class="logo">МКУ "Центр поддержки образования" <br> МО Приморско-Ахтарский район</div>
     </div>
     <div class="body">
-      <div class="side-nav">
+      <div class="aside">
+        <div class="blind-mode">
+          <img src="../assets/slabovidyashie-3.png"/>
+          <label>Версия для слабовидящих</label>
+        </div>
         <MenuTree/>
       </div>
       <div class="content">
-        <!-- <Menu/> -->
+        <PageTitle :title="'Hello world!'"/>
+        <Post/>
       </div>
     </div>
     <div class="foot">
@@ -29,17 +35,17 @@ import Menu from './Menu.vue'
 
 <style scoped>
 
-@import url('https://fonts.googleapis.com/css2?family=EB+Garamond&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;700&family=PT+Sans:wght@400;700&family=PT+Serif:wght@400;700&display=swap');
 
 .page{
   display: block;
   justify-content: center;
-  width: 900px;
+  width: 1000px;
   background-color: bisque;
-  font-family: 'EB Garamond', serif;
+  font-family: 'PT Sans', sans-serif;
 }
 
-.head{
+.top{
   background-color: orange;
 }
 
@@ -48,34 +54,67 @@ import Menu from './Menu.vue'
   display: flex;
   align-items: center;
   text-align: left;
-  height: 200px;
+  height: 150px;
   background: repeat;
-  font-size: 3.2em;
+  font-size: 3em;
+  line-height: normal;
+  margin-left: 0.5em;
 }
 
 .foot{
   color:black;
-  background-color: palevioletred;
+  background-color: orange;
   text-align: center;
 }
 
 .body{
   display: flex;
   min-height: 900px;
+  height: fit-content;
+  font-family: 'PT Serif', serif;
 }
 
-.side-nav{
+.aside{
   width: 230px;
   min-height: 900px;
-  height: 100%;
+  height: fit-content;
   background-color: pink;
+  color: blue;
 }
 
 .content{
   width: calc(100% - 230px);
   min-height: 900px;
-  height: 100%;
+  height: fit-content;
   background-color: greenyellow;
+}
+
+.blind-mode{
+  width: 100%;
+  text-align: center;
+  background-color: white;
+  border: 2px solid gray;
+}
+
+.blind-mode:hover{
+  cursor: pointer;
+  transition: all 200ms;
+  filter: blur(5px);
+}
+
+.blind-mode > img{
+  height: 100px;
+  width: 100%;
+}
+
+.blind-mode > label{
+  font-size: larger;
+  font-family: 'PT Sans', serif;
+  color: #000;
+  line-height: 2em;
+  border: none;
+  outline: none;
+
 }
 
 
